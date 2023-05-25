7d34adb0b6402d6aa7938033290fd1b7d327aa29
ShunIshizeki <shun2000c9@outlook.jp>
2023-05-25 09:20:56 +0900

    feat: 検索結果のスタイルを変更

--- src/components/search/ResultPost.jsx
+++ src/components/search/ResultPost.jsx
@@ -4,62 +4,69 @@ import { useNavigate } from "react-router-dom";
 import sampleIconImg from "../../images/sampleIcon.png";
 
 const ResultPost = ({ posts }) => {
-  //navigateのインスタンス化
   const navigate = useNavigate();
 
   return (
-    <div className="result_post_box">
-      {/* 検索結果 */}
-      {posts.map((post) => (
-        <div
-          key={post._id}
-          className="userInfoBox container-fluid border border-dark p-0 m-0 row"
-          style={{ backgroundColor: "white" }}
-        >
-          {/* ユーザー情報 */}
-          <Button
-            variant="text"
-            className="col-3 d-flex flex-column"
-            onClick={() =>
-              navigate(`/profile/${post.username}`, { state: post })
-            }
+    <div className="result_posts container-fluid p-0 border-bottom border-dark">
+      {/* 投稿 */}
+      <div className="result_posts_box" style={{ flex: 1 }}>
+        {posts.map((post) => (
+          <div
+            key={post._id}
+            className="result_post container-fluid border border-dark p-0 m-0 d-flex"
+            style={{ backgroundColor: "rgba(255,255,255,1)" }}
           >
-            <Avatar
-              alt="profile"
-              src={
-                post.profilePicture
-                  ? process.env.REACT_APP_API_URL +
-                    `/users/profilePicture/${post.profilePicture}`
-                  : sampleIconImg
-              }
-              className="border border-dark m-0"
-            />
-            <Typography
-              gutterBottom
-              variant="subtitle1"
-              component="div"
-              textTransform="none"
-            >
-              {post.username}
-            </Typography>
-          </Button>
-
-          {/* 投稿内容 */}
-          <Button
-            variant="text"
-            sx={{ textTransform: "none" }}
-            className="col-9 d-flex flex-column p-3 m-0"
-            onClick={() => navigate("/view-post", { state: post })}
-          >
-            <p className="sentence text-center mx-auto">
-              <strong>{post.postTitle}</strong>
-            </p>
-            <p className="sentence text-center mx-auto">
-              注目選手：{post.goodPlayer}
-            </p>
-          </Button>
-        </div>
-      ))}
+            {/* ユーザー情報 */}
+            <div className="profile">
+              <Button
+                variant="text"
+                className="post_author pt-1 d-flex justify-content-start ps-2 pt-0 m-0"
+                onClick={() =>
+                  navigate(`/profile/${post.username}`, { state: post })
+                }
+              >
+                <Avatar
+                  alt="profile"
+                  src={
+                    post.profilePicture
+                      ? process.env.REACT_APP_API_URL +
+                        `/users/profilePicture/${post.profilePicture}`
+                      : sampleIconImg
+                  }
+                  className="author border border-dark m-0"
+                />
+              </Button>
+            </div>
+            <div className="name_content flex-grow-1">
+              <Typography
+                gutterBottom
+                variant="p"
+                component="div"
+                textTransform="none"
+                className="post_username"
+              >
+                {post.username}
+              </Typography>
+              {/* 投稿内容 */}
+              <Button
+                variant="text"
+                sx={{ textTransform: "none" }}
+                className="post_content p-0 d-flex flex-column align-items-start w-100"
+                onClick={() => navigate("/view-post", { state: post })}
+              >
+                <p className="sentence m-0 text-dark">
+                  <strong>{post.postTitle}</strong>
+                </p>
+                <p className="sentence m-0 text-dark">
+                  <strong>
+                    <small>注目選手：{post.goodPlayer}</small>
+                  </strong>
+                </p>
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
